a1227dac2b42f0403b64d3105f280ed7a743fc16
Aman Singh <amansinghs7010@gmail.com>
2024-12-01 01:41:21 -0400

    added google auth

--- src/lib/components/AuthForm.svelte
+++ src/lib/components/AuthForm.svelte
@@ -14,7 +14,11 @@
 <div class="default-margin auth-container">
 	<h1 class="mb-l">{isRegistration ? 'Register' : 'Login'}</h1>
 	<div class="form-and-social-login">
-		<form class="auth-form" method="POST">
+		<form
+			class="auth-form"
+			method="POST"
+			action={isRegistration ? '' : '/login/?/signInWithPassword'}
+		>
 			{#if form && form.errors?.length}
 				{#each form.errors as error}
 					<div class="auth-error">
@@ -47,7 +51,9 @@
 			{/if}
 		</form>
 		<div class="social-login">
-			<!--Add a button later-->
+			<form method="POST" action={isRegistration ? '/login/?/googleLogin' : '?/googleLogin'}>
+				<Button type="submit">Log in using Google</Button>
+			</form>
 		</div>
 	</div>
 </div>
